6cc40dc985f841b245cd79a089250092ddc2f489
aantr <xgamestuidio8@gmail.com>
2026-02-20 20:42:58 +0500

    bars

---
+++ main.py
@@ -0,0 +1,32 @@
+'''
+
+generate dataset with level bars 
+train 
+get dataset with picturers from battles
+
+train classification
+
+# todo
+generate dataset with units
+
+yolo spells dataset
+generate dataset with spells
+
+yolo blue units
+# todo
+
+'''
+
+
+def update_screen_frame():
+    pass
+
+    # yolo level bar detection + cnn classification
+    # result = strategy.get()
+    # return result
+
+
+def make_move():
+    pass
+
+
---
+++ train_cnn.py
@@ -0,0 +1,9 @@
+def get_random_unit():
+    # get random screenshot
+    # use model with bars
+    # ask what unit it is 
+    # generate (repeat)
+    pass
+
+def save_result():
+    pass
